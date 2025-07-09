8b015424aece2bd103b608c3c3dcc6fa46b42ecb
steve <steve.berenger@gmail.com>
2025-07-09 15:06:26 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// src/App.tsx
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600 text-center">
+        Bienvenue dans le visualiseur de logs 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
